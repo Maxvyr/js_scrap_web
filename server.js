8ffb1a842bbb1f61de710870270c23b15c04019a
maxvyr <maxvid30@gmail.com>
2021-08-17 14:11:14 +0200

    scrap price

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer');
 
-const url = "https://www.kubii.fr/";
+const url = "https://www.kubii.fr/cartes-raspberry-pi/2771-nouveau-raspberry-pi-4-modele-b-2gb-0765756931175.html?search_query=Pi4&results=111";
 
 (async () => {
     const browser = await puppeteer.launch({headless: true});
@@ -8,21 +8,29 @@ const url = "https://www.kubii.fr/";
     await page.goto(url, {waitUntil: "networkidle2"});
 
     // size viewport for sreenschot
-    await page.setViewport({
-        width: 1500,
-        height: 2500,
-    });
+    // await page.setViewport({
+    //     width: 1500,
+    //     height: 2500,
+    // });
 
-    // pdf
-    await page.pdf({
-        path : "page.pdf",
-        format : "A4",
-    });
+    // // pdf
+    // await page.pdf({
+    //     path : "page.pdf",
+    //     format : "A4",
+    // });
 
-    //image size image = viewport size
-    await page.screenshot({
-        path : "image.png",
+    // //image size image = viewport size
+    // await page.screenshot({
+    //     path : "image.png",
+    // });
+
+    // recover price specific
+    // use itemprop
+    let priceRecover = await page.evaluate(() => {
+        return document.querySelector('span[itemprop=price]').innerHTML;
     });
+    let pricePi4 = priceRecover.substring(0, 2)
+    console.log("le prix est de => " + pricePi4);
 
     // close
     await browser.close();
